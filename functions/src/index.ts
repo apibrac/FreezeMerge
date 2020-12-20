6743b6fc86911fcb9870ad344330e3dc4b7f8c1e
apibrac <alexis.pibrac@gmail.com>
2020-12-20 18:25:14 +0100

    Create empty function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  functions.logger.info("Request", request);
+  functions.logger.info("Request.body", request.body);
+  response.send("Hello pibou!");
+});
