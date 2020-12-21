ff437120696f75c37b665de33a5fc5cc391b81be
apibrac <alexis.pibrac@gmail.com>
2020-12-21 16:31:11 +0100

    Configure environnement variables for Probot

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,15 +2,19 @@ import * as functions from "firebase-functions";
 import appFn from "./probot";
 import { Server, Probot } from "probot";
 
-const appId = "env.APP_ID";
-const privateKey = "env.PRIVATE_KEY";
-const secret = "env.WEBHOOK_SECRET";
-
+const config = functions.config();
 const server = new Server({
-  Probot: Probot.defaults({ appId, privateKey, secret }),
+  Probot: Probot.defaults({
+    appId: config.github.app_id,
+    secret: config.github.webhook_secret,
+    privateKey: Buffer.from(config.github.private_key, "base64").toString(
+      "ascii"
+    ),
+  }),
 });
 
 server
   .load(appFn)
   .catch((error) => console.error(`Error loading probot app ${error}`));
+
 export const github_webhook = functions.https.onRequest(server.expressApp);
