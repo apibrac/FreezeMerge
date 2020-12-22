b55c894c0cbfc38a57b7bf449987c8007dceda26
apibrac <alexis.pibrac@gmail.com>
2020-12-22 13:37:18 +0100

    Retrieve check by id

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,33 @@
 import * as functions from "firebase-functions";
 import appFn from "./probot";
-import { Server, Probot } from "probot";
+import { Server, Probot, ProbotOctokit } from "probot";
 
 const config = functions.config();
-const server = new Server({
-  Probot: Probot.defaults({
-    appId: config.github.app_id,
-    secret: config.github.webhook_secret,
-    privateKey: Buffer.from(config.github.private_key, "base64").toString(
-      "ascii"
-    ),
-  }),
+const Octokit = ProbotOctokit.defaults({
+  appId: config.github.app_id,
+  secret: config.github.webhook_secret,
+  privateKey: Buffer.from(config.github.private_key, "base64").toString(
+    "ascii"
+  ),
 });
 
+const server = new Server({ Probot: Probot.defaults({ Octokit }) });
 server
   .load(appFn)
   .catch((error) => console.error(`Error loading probot app ${error}`));
 
 export const github_webhook = functions.https.onRequest(server.expressApp);
+
+export const getCheckById = functions.https.onRequest(async (req, resp) => {
+  const id = req.query.id;
+  const checkRunId = typeof id === "string" ? parseInt(id) : 0;
+  const octokit = new Octokit();
+
+  const check = await octokit.checks.get({
+    check_run_id: checkRunId,
+    owner: "apibrac",
+    repo: "FreezeMerge",
+  });
+  functions.logger.info(check);
+  resp.send(check);
+});
--- functions/src/probot.ts
+++ functions/src/probot.ts
@@ -3,37 +3,28 @@ import { Probot } from "probot";
 
 export default (app: Probot) => {
   app.on(
-    ["check_suite.requested", "check_run.rerequested"],
+    ["check_suite.requested"], // , "check_run.rerequested"
     async function (context) {
-      logger.info("Check context payload", context.payload);
-      // const startTime = new Date();
+      const startTime = new Date();
 
-      // const {
-      //   head_branch: headBranch,
-      //   head_sha: headSha,
-      // } = context.payload.check_suite;
-      // // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
-      // return context.octokit.checks.create(
-      //   context.repo({
-      //     name: "My app!",
-      //     head_branch: headBranch,
-      //     head_sha: headSha,
-      //     status: "completed",
-      //     started_at: startTime,
-      //     conclusion: "success",
-      //     completed_at: new Date(),
-      //     output: {
-      //       title: "Probot check!",
-      //       summary: "The check has passed!",
-      //     },
-      //   })
-      // );
+      const {
+        head_branch: headBranch,
+        head_sha: headSha,
+      } = context.payload.check_suite;
+
+      const check = await context.octokit.checks.create(
+        context.repo({
+          name: "Freeze Merge",
+          head_branch: headBranch,
+          head_sha: headSha,
+          status: "completed",
+          started_at: startTime.toISOString(),
+          conclusion: "success",
+          completed_at: new Date().toISOString(),
+        })
+      );
+      logger.info("checkId", check.data.id);
+      return check;
     }
   );
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
