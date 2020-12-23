9e37c442e13aeb26792f37cb0e46f4d86c6954c3
apibrac <alexis.pibrac@gmail.com>
2020-12-24 00:28:57 +0100

    Put back live check when opening PR

--- firebase/functions/src/probot.ts
+++ firebase/functions/src/probot.ts
@@ -1,5 +1,6 @@
 import { Probot } from "probot";
 import admin from "firebase-admin";
+import { logger } from "firebase-functions";
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -37,6 +38,49 @@ export default (app: Probot) => {
     return installation.collection("checks").add(
       context.repo({
         check_run_id: check.data.id,
+      })
+    );
+  });
+
+  app.on(["pull_request.opened"], async function (context) {
+    logger.info("Pull request opened");
+    const headSha = context.payload.pull_request.head.sha;
+
+    const installation_id = context.payload.installation?.id;
+    if (!installation_id)
+      throw new Error("No installation found for this check");
+
+    const installation = db
+      .collection("installations")
+      .doc(installation_id.toString());
+    const data = (await installation.get()).data();
+    if (!data) throw new Error("Did not found installation");
+
+    const checksRequest = await context.octokit.checks.listForRef(
+      context.repo({
+        ref: headSha,
+      })
+    );
+    const checkRuns = checksRequest.data.check_runs;
+    if (checkRuns.length === 0) {
+      throw new Error("No check on this PR head");
+    }
+    if (checkRuns.length > 1) {
+      logger.warn(
+        "Case not implemented: Shouldn't have more than one check on a commit"
+      );
+    }
+    const check = checkRuns[0];
+    await context.octokit.checks.update(
+      context.repo({
+        check_run_id: check.id,
+        conclusion: data.freezed ? "failure" : "success",
+      })
+    );
+
+    return installation.collection("checks").add(
+      context.repo({
+        check_run_id: check.id,
       })
     );
   });
