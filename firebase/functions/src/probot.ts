71bbfbcfbca9436953554762fe66dbfa97e1c4c7
apibrac <alexis.pibrac@gmail.com>
2020-12-23 11:24:31 +0100

    Save check values to be updated later

--- firebase/functions/src/probot.ts
+++ firebase/functions/src/probot.ts
@@ -1,30 +1,41 @@
-import { logger } from "firebase-functions";
 import { Probot } from "probot";
+import admin from "firebase-admin";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+const ACCOUNT_ID = "pass-culture";
 
 export default (app: Probot) => {
-  app.on(
-    ["check_suite.requested"], // , "check_run.rerequested"
-    async function (context) {
-      const startTime = new Date();
+  app.on(["check_suite.requested"], async function (context) {
+    const startTime = new Date();
 
-      const {
+    const {
+      head_branch: headBranch,
+      head_sha: headSha,
+    } = context.payload.check_suite;
+
+    const account = db.collection("account").doc(ACCOUNT_ID);
+    const data = (await account.get()).data();
+    if (!data) throw new Error("Did not found account");
+    data.freezed;
+
+    const check = await context.octokit.checks.create(
+      context.repo({
+        name: "Freeze Merge",
         head_branch: headBranch,
         head_sha: headSha,
-      } = context.payload.check_suite;
+        status: "completed",
+        started_at: startTime.toISOString(),
+        conclusion: data.freezed ? "failure" : "success",
+        completed_at: new Date().toISOString(),
+      })
+    );
 
-      const check = await context.octokit.checks.create(
-        context.repo({
-          name: "Freeze Merge",
-          head_branch: headBranch,
-          head_sha: headSha,
-          status: "completed",
-          started_at: startTime.toISOString(),
-          conclusion: "success",
-          completed_at: new Date().toISOString(),
-        })
-      );
-      logger.info("checkId", check.data.id);
-      return check;
-    }
-  );
+    return account.collection("checks").add(
+      context.repo({
+        check_run_id: check.data.id,
+      })
+    );
+  });
 };
