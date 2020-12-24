1bff0fbfa63bd3c6d2d80f876f079d11fc5f738c
apibrac <alexis.pibrac@gmail.com>
2020-12-24 08:51:55 +0100

    Correct installation only on head of PR

--- firebase/functions/src/probot.ts
+++ firebase/functions/src/probot.ts
@@ -12,19 +12,19 @@ export type Installation = {
 
 export function isCheckFreezed(
   installation: Installation,
-  check?: {
+  check: {
     pull_requests: { url: string; head: { sha: string } }[];
     head_sha: string;
   }
 ) {
   if (!installation.freezed) return false;
-  if (check) {
-    const pull_requests = check.pull_requests.filter(
-      ({ head }) => head.sha === check.head_sha
-    );
-    if (pull_requests.find((pr) => installation.whitelist.includes(pr.url)))
-      return false;
-  }
+
+  const pull_requests = check.pull_requests.filter(
+    ({ head }) => head.sha === check.head_sha
+  );
+  if (pull_requests.find((pr) => installation.whitelist.includes(pr.url)))
+    return false;
+
   return true;
 }
 
@@ -32,10 +32,7 @@ export default (app: Probot) => {
   app.on(["check_suite.requested"], async function (context) {
     const startTime = new Date();
 
-    const {
-      head_branch: headBranch,
-      head_sha: headSha,
-    } = context.payload.check_suite;
+    const checkSuite = context.payload.check_suite;
     const installation_id = context.payload.installation?.id;
     if (!installation_id)
       throw new Error("No installation found for this check");
@@ -48,11 +45,11 @@ export default (app: Probot) => {
     const check = await context.octokit.checks.create(
       context.repo({
         name: "Freeze Merge",
-        head_branch: headBranch,
-        head_sha: headSha,
+        head_branch: checkSuite.head_branch,
+        head_sha: checkSuite.head_sha,
         status: "completed",
         started_at: startTime.toISOString(),
-        conclusion: isCheckFreezed(data) ? "failure" : "success",
+        conclusion: isCheckFreezed(data, checkSuite) ? "failure" : "success",
         completed_at: new Date().toISOString(),
       })
     );
@@ -64,45 +61,48 @@ export default (app: Probot) => {
     );
   });
 
-  app.on(["pull_request.opened"], async function (context) {
-    logger.info("Pull request opened");
-    const headSha = context.payload.pull_request.head.sha;
+  app.on(
+    ["pull_request.opened", "pull_request.reopened"],
+    async function (context) {
+      logger.info("Pull request opened");
+      const headSha = context.payload.pull_request.head.sha;
 
-    const installation_id = context.payload.installation?.id;
-    if (!installation_id)
-      throw new Error("No installation found for this check");
+      const installation_id = context.payload.installation?.id;
+      if (!installation_id)
+        throw new Error("No installation found for this check");
 
-    const installation = db
-      .collection("installations")
-      .doc(installation_id.toString());
-    const data = (await installation.get()).data() as Installation;
+      const installation = db
+        .collection("installations")
+        .doc(installation_id.toString());
+      const data = (await installation.get()).data() as Installation;
 
-    const checksRequest = await context.octokit.checks.listForRef(
-      context.repo({
-        ref: headSha,
-      })
-    );
-    const checkRuns = checksRequest.data.check_runs;
-    if (checkRuns.length === 0) {
-      throw new Error("No check on this PR head");
-    }
-    if (checkRuns.length > 1) {
-      logger.warn(
-        "Case not implemented: Shouldn't have more than one check on a commit"
+      const checksRequest = await context.octokit.checks.listForRef(
+        context.repo({
+          ref: headSha,
+        })
+      );
+      const checkRuns = checksRequest.data.check_runs;
+      if (checkRuns.length === 0) {
+        throw new Error("No check on this PR head");
+      }
+      if (checkRuns.length > 1) {
+        logger.warn(
+          "Case not implemented: Shouldn't have more than one check on a commit"
+        );
+      }
+      const check = checkRuns[0];
+      await context.octokit.checks.update(
+        context.repo({
+          check_run_id: check.id,
+          conclusion: isCheckFreezed(data, check) ? "failure" : "success",
+        })
+      );
+
+      return installation.collection("checks").add(
+        context.repo({
+          check_run_id: check.id,
+        })
       );
     }
-    const check = checkRuns[0];
-    await context.octokit.checks.update(
-      context.repo({
-        check_run_id: check.id,
-        conclusion: isCheckFreezed(data, check) ? "failure" : "success",
-      })
-    );
-
-    return installation.collection("checks").add(
-      context.repo({
-        check_run_id: check.id,
-      })
-    );
-  });
+  );
 };
