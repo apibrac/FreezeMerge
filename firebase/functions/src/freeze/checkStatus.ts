683b8edcadffec775a5c1dd5ff1f84bf73cf43a4
apibrac <alexis.pibrac@gmail.com>
2021-01-10 19:17:33 +0100

    Add new statuses for special cases

--- firebase/functions/src/freeze/checkStatus.ts
+++ firebase/functions/src/freeze/checkStatus.ts
@@ -9,15 +9,45 @@ export const checkRunStatus = {
   success: (): CheckAttributes => ({
     conclusion: "success",
     completed_at: new Date().toISOString(),
+    output: {
+      title: "OK",
+      summary: "Freeze not running",
+    },
   }),
 
   freezed: (): CheckAttributes => ({
     conclusion: "failure",
     completed_at: new Date().toISOString(),
+    output: {
+      title: "FREEZED",
+      summary: "Not mergable, freeze is running",
+    },
   }),
 
   notSynced: (): CheckAttributes => ({
     conclusion: "success",
     completed_at: new Date().toISOString(),
+    output: {
+      title: "Outdated",
+      summary: "This commit is not ctracked anymore",
+    },
+  }),
+
+  whtelistedPullRequest: (number: number): CheckAttributes => ({
+    conclusion: "success",
+    completed_at: new Date().toISOString(),
+    output: {
+      title: "Whitelisted PR",
+      summary: `This commit is whitelisted because PR #${number} is.`,
+    },
+  }),
+
+  whitelistedTicket: (tag: string): CheckAttributes => ({
+    conclusion: "success",
+    completed_at: new Date().toISOString(),
+    output: {
+      title: "Whitelisted ticket",
+      summary: `This commit is whitelisted because ticket ${tag} is.`,
+    },
   }),
 };
