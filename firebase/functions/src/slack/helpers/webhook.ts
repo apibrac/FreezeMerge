404200e6c81b3294d584fdfd0998bef2aab7305e
Justine Mignot <justinem@bam.tech>
2021-02-01 20:16:47 +0100

    :sparkles: (slack-integration) commands only launchable in specific channel

--- firebase/functions/src/slack/helpers/webhook.ts
+++ firebase/functions/src/slack/helpers/webhook.ts
@@ -2,6 +2,12 @@ import express from "express";
 import cors from "cors";
 import * as functions from "firebase-functions";
 
+const SLACK_CHANNEL_ACCEPTED = 'freeze_merge'
+const error_message = {
+  "response_type": "in_channel",
+  "text": `[ERREUR] Interdiction d'utiliser FreezeMerge dans ce channel. Il est utilisable uniquement dans le channel ${SLACK_CHANNEL_ACCEPTED}. Si vous n'y avez pas accès et en avez besoin, demandez à un responsable technique.`,
+}
+
 export function slackWebhook(action: (id: string, text: string) => any) {
   const app = express();
 
@@ -9,6 +15,10 @@ export function slackWebhook(action: (id: string, text: string) => any) {
   app.use(express.urlencoded({ extended: true }));
 
   app.post("/:id", async (req, res) => {
+    if (req.body.channel_name !== SLACK_CHANNEL_ACCEPTED) {
+      // We must return a 200 otherwise slack display a not explicit error that we can't customize
+      return res.status(200).send(error_message)
+    }
     const message = await action(req.params.id, req.body.text);
     return res.status(200).send(message);
   });
