e89323f9d5658576b3c7f2b645baf5d7098647f7
apibrac <alexis.pibrac@gmail.com>
2021-02-02 08:31:17 +0100

    Rename collections before redeploy

--- firebase/functions/src/freeze/config.ts
+++ firebase/functions/src/freeze/config.ts
@@ -1,7 +1,7 @@
 import admin from "firebase-admin";
 
-export const PERSISTENCES = "installations";
-export const HOOKS = "checks";
+export const PERSISTENCES = "controllers";
+export const HOOKS = "hooks";
 
 admin.initializeApp();
 export const db = admin.firestore();
